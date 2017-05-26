3ac1bcc50017df5a461828016fff738a16e49886
johnneycat <qt19911220@qq.com>
2017-05-26 18:32:43 +0800

    增加了一些操作
    喂狗的游戏

--- struct_func.c
+++ struct_func.c
@@ -5,28 +5,70 @@
 #include <stdlib.h>
 #include <linux/sysctl.h>
 
-typedef struct HELLO
+
+
+struct RECT_BOWL
 {
-	char *c;
-	int a;
-	int (*func)(int);
+    int x;
+    int y;
 };
 
-int world(int c)
+struct ROUND_BOWL
 {
-	printf("%d\n", c);
+    int diameter;
+};
+
+//feed a dog
+typedef struct FeedDog {
+    void* bowl;
+    int is_round;
+    char* dog_name;
+    int (*get_bowl)(struct FeedDog*);
+    int (*put_food)(char*);
+};
+
+//i wanna to get a bowl to obtain the food
+int get_bowl(struct FeedDog* fdg)
+{
+    if(!fdg){
+        printf("%s\n", "I donnt think you wanna to feed dog, please go away.\n");
+        return -1;
+    }
+    
+    //round bowl is cheaper
+    fdg->bowl = (void*)malloc(sizeof(struct ROUND_BOWL));
+    if(fdg->bowl) {
+        ((struct ROUND_BOWL*)(fdg->bowl))->diameter = 10;
+        fdg->is_round = 1;
+        printf("%s\n", "get a round bowl\n");
+        return 0;
+    }
+    //i only get a rectangle bowl
+    else {
+        fdg->bowl = (void*)malloc(sizeof(struct RECT_BOWL));
+        ((struct RECT_BOWL*)(fdg->bowl))->x = 10;
+        ((struct RECT_BOWL*)(fdg->bowl))->y = 20;
+        fdg->is_round = 0;
+        printf("%s\n", "get a rect bowl\n");
+        return 0;
+    }
 }
 
-struct HELLO hi = {
-	.c = "good morning",
-	.func = world,
+int put_food (char* food)
+{
+    printf("%s is in bowl\n", food);
+}
+
+struct FeedDog IFD = {
+    .dog_name = "AnPie",
+    .get_bowl = get_bowl,
+    .put_food = put_food,
 };
 
 int main()
 {
-	// struct HELLO hello;
-	// hello.func = world;
-	// hello.func(10);
-	hi.func(10);
-	return 0;
+    IFD.get_bowl(&IFD);
+    IFD.put_food("pumpkin");
+    printf("bowl: %d\n", IFD.is_round);
+    return 0;
 }
